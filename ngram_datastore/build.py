3dfb1362de6fe1c92521ae4ddb840e5b11836956
soleee99 <soleeep99@gmail.com>
2024-04-06 20:10:13 +0000

    short options

--- ngram_datastore/build.py
+++ ngram_datastore/build.py
@@ -8,9 +8,9 @@ from ngram_datastore.ngram_datastore import NGramDatastore
 @click.option("--model-path", type=str, default="lmsys/vicuna-7b-v1.5")
 @click.option("--dataset-name", type=str, default="Aeala/ShareGPT_Vicuna_unfiltered")
 @click.option("--datastore-path", type=str, default="./datastore/datastore_chat_small.idx")
-@click.option("--ngram-n", type=int, default=3)
-@click.option("--num-conversations", type=int, default=10)
-@click.option("--num-top-ngrams", type=int, default=10)
+@click.option("--ngram-n", "-n", type=int, default=3)
+@click.option("--num-conversations", "-c", type=int, default=10)
+@click.option("--num-top-ngrams", "-t", type=int, default=10)
 def main(model_path: str, dataset_name: str, datastore_path: str, ngram_n: int, num_conversations: int, num_top_ngrams: int):
     reader = draftretriever.Reader(
         index_file_path=datastore_path,
