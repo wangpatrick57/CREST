846088aa92920d18004cb7661d68b44091b9d19c
soleee99 <soleeep99@gmail.com>
2024-04-07 20:15:55 +0000

    add include-all option for building datastore

--- ngram_datastore/build.py
+++ ngram_datastore/build.py
@@ -8,14 +8,16 @@ from ngram_datastore.ngram_datastore import NGramDatastoreBuilder
 @click.option("--model-path", type=str, default="lmsys/vicuna-7b-v1.5")
 @click.option("--dataset-name", type=str, default="Aeala/ShareGPT_Vicuna_unfiltered")
 @click.option("--datastore-path", type=str, default="./datastore/datastore_chat_small.idx")
-@click.option("--ngram-n", "-n", type=int, default=3)
-@click.option("--num-conversations", "-c", type=int, default=10)
-@click.option("--num-top-ngrams", "-t", type=int, default=10)
-def main(model_path: str, dataset_name: str, datastore_path: str, ngram_n: int, num_conversations: int, num_top_ngrams: int):
+@click.option("--ngram-n", "-n", type=int, default=3)               # number of ngrams to build the datastore on
+@click.option('--include-all', '-a', is_flag=True)                  # includes all ngrams up to ngram_n. Specify either num_top_ngrams or merge_ratio for keeping only a few when merging
+@click.option("--num-conversations", "-c", type=int, default=10)    # number of conversations to build the datstore
+@click.option("--num-top-ngrams", "-t", type=int, default=10)       # for keeping in the datastore
+@click.option('--merge-ratio', '-r',type=float, default=0.0)        # merge ratio. If not specified, merging defaults to choosing top N
+def main(model_path: str, dataset_name: str, datastore_path: str, ngram_n: int, num_conversations: int, num_top_ngrams: int, include_all: bool, merge_ratio: float):
     reader = draftretriever.Reader(
         index_file_path=datastore_path,
     )
-    datastore_builder = NGramDatastoreBuilder(dataset_name, num_conversations, model_path, reader, ngram_n, num_top_ngrams)
+    datastore_builder = NGramDatastoreBuilder(dataset_name, num_conversations, model_path, reader, ngram_n, num_top_ngrams, include_all, merge_ratio)
     datastore = datastore_builder.load_or_build()
 
 if __name__ == '__main__':
