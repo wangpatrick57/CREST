3c75da33cc22d9c6c62f7c308eec0e27315e3ffc
Patrick Wang <wang.patrick57@gmail.com>
2024-04-08 16:23:34 -0400

    settings class

--- ngram_datastore/ngram_datastore.py
+++ ngram_datastore/ngram_datastore.py
@@ -9,6 +9,16 @@ import time
 import pickle
 import lzma
 import psycopg2
+
+
+class NGramDatastoreSettings:
+    def __init__(self, dataset_name: str, ngram_n: int, include_all: bool, num_conversations: int, num_top_ngrams: int, merge_ratio: float):
+        self.dataset_name = dataset_name
+        self.ngram_n = ngram_n
+        self.include_all = include_all
+        self.num_conversations = num_conversations
+        self.num_top_ngrams = num_top_ngrams
+        self.merge_ratio = merge_ratio
         
 
 class NGramDatastore:
