a3222fcab27ec6e6440f38edf322c95bb20cc5fe
Patrick Wang <wang.patrick57@gmail.com>
2024-04-08 16:26:11 -0400

    moved settings to its own file

--- ngram_datastore/ngram_datastore.py
+++ ngram_datastore/ngram_datastore.py
@@ -4,21 +4,10 @@ from ngram_datastore.utils import *
 from transformers import AutoTokenizer
 from tqdm import tqdm
 
-import os
 import time
 import pickle
 import lzma
 import psycopg2
-
-
-class NGramDatastoreSettings:
-    def __init__(self, dataset_name: str, ngram_n: int, include_all: bool, num_conversations: int, num_top_ngrams: int, merge_ratio: float):
-        self.dataset_name = dataset_name
-        self.ngram_n = ngram_n
-        self.include_all = include_all
-        self.num_conversations = num_conversations
-        self.num_top_ngrams = num_top_ngrams
-        self.merge_ratio = merge_ratio
         
 
 class NGramDatastore:
