28af55b963f650c520f50727df7e31d049874e62
Ubuntu <ubuntu@ip-172-31-23-53.ec2.internal>
2024-04-10 12:47:07 +0000

    changed individual n test points

--- experiments/individual_n_tests.sh
+++ experiments/individual_n_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-ngram_ns=(1 2 3 4 5 6)
-merge_ratios=(0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1 0.12 0.14 0.16 0.18 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 0)
+ngram_ns=(2 3 4 5 6)
+merge_ratios=(0.01 0.02 0.04 0.08 0.16 0.32 0.64 0)
 
 for ngram_n in "${ngram_ns[@]}"; do
     for merge_ratio in "${merge_ratios[@]}"; do
