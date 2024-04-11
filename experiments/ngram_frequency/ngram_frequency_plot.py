ac22516bbcbc611fc853f16a9fb09565f1e8ecdc
soleee99 <soleeep99@gmail.com>
2024-04-11 04:37:35 +0000

    frequency plots for ~4gram

--- experiments/ngram_frequency/ngram_frequency_plot.py
+++ experiments/ngram_frequency/ngram_frequency_plot.py
@@ -1,60 +1,94 @@
 import matplotlib.pyplot as plt
 import pickle
 from itertools import islice
+import numpy as np
 
 # with open("./frequency_data/sharegpt-4gram-frequency-top1000.pkl", 'wb') as f:
 #     pickle.dump(dict(islice(four_grams.items(), 1000)), f)
 # quit()
+plt.figure(figsize=(8, 4))
+
 
 with open("./frequency_data/sharegpt-1gram-frequency.pkl", 'rb') as f:
-    one_grams = pickle.load(f)
-    # print(len(one_grams.keys()))
-print("load 1-gram")
-with open("./frequency_data/sharegpt-2gram-frequency.pkl", 'rb') as f:
-    two_grams = pickle.load(f)
-print("load 2-gram")
-with open("./frequency_data/sharegpt-3gram-frequency.pkl", 'rb') as f:
-    three_grams = pickle.load(f)
-print("load 3-gram")
-with open("./frequency_data/sharegpt-4gram-frequency-top1000.pkl", 'rb') as f:
-    four_grams = pickle.load(f)
-print("load 4-gram")
-with open("./frequency_data/sharegpt-5gram-frequency-top10000.pkl", 'rb') as f:
-    five_grams = pickle.load(f)
-print("load 5-gram")
-with open("./frequency_data/sharegpt-6gram-frequency-top10000.pkl", 'rb') as f:
-    six_grams = pickle.load(f)
-print("load 6-gram")
+    grams = pickle.load(f)
 
-
-plt.figure(figsize=(10, 4))
-topk = 100
-x_axis = [i for i in range(topk)]
-plt.subplot(2, 3, 1)
-plt.bar(x_axis, list(one_grams.values())[:topk])
+plt.subplot(2, 2, 1)
+val = np.array(list(grams.values()))
+x_axis = [i for i in range(len(val))]
+total = np.sum(val)
+cum = np.cumsum(val / total)
+plt.plot(x_axis, cum)
+plt.xlabel("number of 1grams")
 plt.title("1-grams")
-plt.ylabel("Frequency")
+plt.xticks(fontsize="x-small")
+plt.ylabel("Frequency ratio")
+print(f"load 1-gram, {len(val)}")
 
-plt.subplot(2, 3, 2)
-plt.bar(x_axis, list(two_grams.values())[:topk])
+with open("./frequency_data/sharegpt-2gram-frequency.pkl", 'rb') as f:
+    grams = pickle.load(f)
+plt.subplot(2, 2, 2)
+val = np.array(list(grams.values()))
+x_axis = [i for i in range(len(val))]
+total = np.sum(val)
+cum = np.cumsum(val / total)
+plt.plot(x_axis, cum)
+plt.xlabel("number of 2grams")
 plt.title("2-grams")
+plt.xticks(fontsize="x-small")
+print(f"load 2-gram, {len(val)}")
 
-plt.subplot(2, 3, 3)
-plt.bar(x_axis, list(three_grams.values())[:topk])
+with open("./frequency_data/sharegpt-3gram-frequency.pkl", 'rb') as f:
+    grams = pickle.load(f)
+plt.subplot(2, 2, 3)
+val = np.array(list(grams.values()))
+x_axis = [i for i in range(len(val))]
+total = np.sum(val)
+cum = np.cumsum(val / total)
+plt.plot(x_axis, cum)
+plt.xlabel("number of 3grams")
 plt.title("3-grams")
+plt.xticks(fontsize="x-small")
+print(f"load 3-gram, {len(val)}")
 
-plt.subplot(2, 3, 4)
-plt.bar(x_axis, list(four_grams.values())[:topk])
+with open("./frequency_data/sharegpt-4gram-frequency.pkl", 'rb') as f:
+    grams = pickle.load(f)
+plt.subplot(2, 2, 4)
+val = np.array(list(grams.values()))
+x_axis = [i for i in range(len(val))]
+total = np.sum(val)
+cum = np.cumsum(val / total)
+plt.plot(x_axis, cum)
+plt.xlabel("number of 4grams")
 plt.title("4-grams")
-plt.ylabel("Frequency")
+plt.xticks(fontsize="x-small")
+print(f"load 4-gram, {len(val)}")
 
-plt.subplot(2, 3, 5)
-plt.bar(x_axis, list(five_grams.values())[:topk])
-plt.title("5-grams")
+# with open("./frequency_data/sharegpt-5gram-frequency.pkl", 'rb') as f:
+#     grams = pickle.load(f)
+# plt.subplot(2, 3, 5)
+# val = np.array(list(grams.values()))
+# x_axis = [i for i in range(len(val))]
+# total = np.sum(val)
+# cum = np.cumsum(val / total)
+# plt.plot(x_axis, cum)
+# plt.xlabel("number of 5grams")
+# plt.title("5-grams")
+# plt.xticks(fontsize="x-small")
+# print(f"load 5-gram, {len(val)}")
 
-plt.subplot(2, 3, 6)
-plt.bar(x_axis, list(six_grams.values())[:topk])
-plt.title("6-grams")
+# with open("./frequency_data/sharegpt-6gram-frequency.pkl", 'rb') as f:
+#     grams = pickle.load(f)
+# plt.subplot(2, 3, 6)
+# val = np.array(list(grams.values()))
+# x_axis = [i for i in range(len(val))]
+# total = np.sum(val)
+# cum = np.cumsum(val / total)
+# plt.plot(x_axis, cum)
+# plt.xlabel("number of 6grams")
+# plt.title("6-grams")
+# plt.xticks(fontsize="x-small")
+# print(f"load 6-gram, {len(val)}")
+
 
 plt.tight_layout()
-plt.savefig("./ngram_frequency.png")
+plt.savefig("./ngram_frequency_ratio.png")
