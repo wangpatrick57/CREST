fba8c3cb5b8cd542132bfd3f21aa294df4c58703
Patrick Wang <wang.patrick57@gmail.com>
2024-04-21 20:46:22 -0400

    average_tree_sizes.py

---
+++ experiments/average_tree_sizes.py
@@ -0,0 +1,30 @@
+import lzma
+import pickle
+import sys
+import draftretriever
+import psycopg2
+
+if __name__ == "__main__":
+    dbnames = ["sharegpt_n1_convs0_top1597221", "sharegpt_n2_convs0_top1643949"]
+
+    for dbname in dbnames:
+        conn = psycopg2.connect(
+            dbname=dbname,
+            user="rest_user",
+            password="rest_password",
+            host="localhost",
+            port=5432
+        )
+        cursor = conn.cursor()
+        cursor.execute("SELECT compressed_pickled_tree FROM ngram_datastore ORDER BY RANDOM() LIMIT 100000;")
+        compressed_pickled_tree_rows = cursor.fetchall()
+        num_tokens_list = []
+
+        for compressed_pickled_tree_row in compressed_pickled_tree_rows:
+            compressed_pickled_tree = compressed_pickled_tree_row[0]
+            _, _, tree_indices, _, _ = pickle.loads(lzma.decompress(compressed_pickled_tree))
+            num_tokens = len(tree_indices) - 1
+            num_tokens_list.append(num_tokens)
+        
+        average_num_tokens = sum(num_tokens_list) / len(num_tokens_list)
+        print(f"{dbname} average: {average_num_tokens}")
